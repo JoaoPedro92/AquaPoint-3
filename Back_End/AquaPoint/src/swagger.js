c40cbcc89318f75d482dd3bc4df0741190e03275
João <jpresourcesfivem@gmail.com>
2026-09-22 21:00:38 +0100

    Add Docker CI workflow

--- Back_End/AquaPoint/src/swagger.js
+++ Back_End/AquaPoint/src/swagger.js
@@ -8,7 +8,7 @@ const doc = {
   host: 'localhost:3000'
 }
 
-const outputFile = './swagger.json'
-const routes = ['./app.js']
+const outputFile = './src/swagger.json'
+const routes = ['./src/app.js']
 
 swaggerAutogen({ openapi: '3.0.0' })(outputFile, routes, doc)
